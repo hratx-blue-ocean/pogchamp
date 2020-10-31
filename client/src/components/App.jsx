4da6e65f63ce22ca573e23e8c311b271dd369850
Daniel Leal <leadaniel456@gmail.com>
2020-10-31 16:33:54 -0500

    Functionality to add players name in the proper structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SwissController from './SwissController.jsx';
+import PopulateForm from './PopulateForm.jsx';
 
 const App = () => {
   return (
     <div>
       <h1>Pogchamp</h1>
       <SwissController />
+      <PopulateForm />
     </div>
   );
 }
