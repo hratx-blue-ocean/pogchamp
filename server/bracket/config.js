0f3d4f2f77b477dc17477063bbcfdc384c49723e
gitHandle <earturoga@gmail.com>
2020-10-31 16:37:18 -0600

    bracket component/api

---
+++ server/bracket/config.js
@@ -0,0 +1,1 @@
+//some git ignore things here
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const api = require('./bracket/bracketIndex'); //bracket
 
 const app = express();
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../client/dist');
 
+app.use(express.json()); //bracket
 app.use(express.static(DIST_DIR));
+
+app.use('/api', api) //bracket
 
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
