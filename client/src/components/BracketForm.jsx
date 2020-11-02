3725d85cc1054c7cc435724b0d54a5ffd517eb2a
Emily White <13emwhite13@gmail.com>
2020-11-02 15:30:02 -0600

    added some bracket form styling

--- client/src/components/BracketForm.jsx
+++ client/src/components/BracketForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { Container, Grid, Button, TextField, FormControl } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import './BracketForm.css';
 
 const BracketForm = () => {
 
@@ -36,56 +37,77 @@ const BracketForm = () => {
   const handleAddingPlayers = (e) => {
     e.preventDefault();
 
-    let player =  playerName.current.value;
+    let player = playerName.current.value;
     // console.log('this is the entire', player);
 
-    setPlayers({ participants: [...playersInTournament.participants, {name: player}]});
+    setPlayers({ participants: [...playersInTournament.participants, { name: player }] });
 
-    console.log("these are all the players" , playersInTournament);
+    console.log("these are all the players", playersInTournament);
   }
 
 
   return (
     <Container maxWidth="lg" className="bracketForm">
-    <h1>Bracket</h1>
-    <div>
-      <h4>Tournament Name: {bracketDetails.tournamentName}</h4>
-      <h4>Game Name: {bracketDetails.gameName}</h4>
-      <h4>Number of Players: {bracketDetails.numberOfPlayers}</h4>
-      <h4>Prize Amount: {bracketDetails.prizeAmount}</h4>
-    </div>
-
-    <form noValidate autoComplete="off" onSubmit={handleSubmit} className="setup-form">
-      <TextField label="tournament name" inputRef={tournament}/>
-      <TextField label="game name" inputRef={game}/>
-      <TextField label="number of players" inputRef={players}/>
-      <TextField label="prize amount" inputRef={prize}/>
-      <Button type="submit">Submit</Button>
-    </form>
-
-    <form noValidate autoComplete="off" onSubmit={handleAddingPlayers} className="setup-form">
-      <TextField label="player name" inputRef={playerName}/>
-      <Button type="submit">Add</Button>
-    </form>
-
-    {
-      playersInTournament.participants === []
-      ? ''
-      :
+      <h1>Bracket</h1>
       <div>
-        {playersInTournament.participants.map(player => {
-          return (
-            <p>
-              <h4>{player.name}</h4> <DeleteForeverIcon/>
-           </p>
-          )
-        })}
+        <h4>Tournament Name: {bracketDetails.tournamentName}</h4>
+        <h4>Game Name: {bracketDetails.gameName}</h4>
+        <h4>Number of Players: {bracketDetails.numberOfPlayers}</h4>
+        <h4>Prize Amount: {bracketDetails.prizeAmount}</h4>
       </div>
-    }
-  </Container>
 
-)
+      <form noValidate autoComplete="off" onSubmit={handleSubmit} className="setup-form">
+        <TextField label="tournament name" inputRef={tournament} />
+        <TextField label="game name" inputRef={game} />
+        <TextField label="number of players" inputRef={players} />
+        <TextField label="prize amount" inputRef={prize} />
+        <Button type="submit">Submit</Button>
+      </form>
+
+      <form noValidate autoComplete="off" onSubmit={handleAddingPlayers} className="setup-form">
+        <TextField label="player name" inputRef={playerName} />
+        <Button type="submit">Add</Button>
+      </form>
+
+      {
+        playersInTournament.participants === []
+          ? ''
+          :
+          <div>
+            {playersInTournament.participants.map(player => {
+              return (
+                <Grid
+                  container
+                  direction="row"
+                  justify="space-between"
+                  alignItems="center"
+                  className="bracket-player"
+                >
+                  <p>
+                    <h4>{player.name}</h4> <DeleteForeverIcon/>
+                  </p>
+
+                </Grid>
+              )
+            })}
+          </div>
+      }
+    </Container>
+
+  )
 }
 
 export default BracketForm;
 
+{/* <Grid
+  container
+  direction="row"
+  justify="space-between"
+  alignItems="center"
+  className="bracket-player"
+>
+  <p>
+    <h4>{player.name}</h4> <DeleteForeverIcon />
+  </p>
+
+</Grid> */}
