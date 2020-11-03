d04d1386faf45533b57d4b3d1d25683a0298f85c
Grant Harris <grantxvx@gmail.com>
2020-11-03 14:27:43 -0600

    destructure imports

--- client/src/components/OrganizerDashboard/TournamentHistory/TournamentHistory.jsx
+++ client/src/components/OrganizerDashboard/TournamentHistory/TournamentHistory.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
+import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import {Link, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+
 // import Title from './Title';
 
 // Generate Order Data
