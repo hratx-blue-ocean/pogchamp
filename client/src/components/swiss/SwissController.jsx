97adf4d49bdf50541fe96531457d29829a9af459
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-11-03 13:22:30 -0600

    Add basic rounds chart feature

--- client/src/components/swiss/SwissController.jsx
+++ client/src/components/swiss/SwissController.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useRef } from 'react';
 import { Container, Grid, Button, TextField, FormControl } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 import './SwissController.css';
 
 const SwissController = (props) => {
@@ -12,7 +19,9 @@ const SwissController = (props) => {
   });
 
   const [playerInfo, setPlayerInfo] = useState({});
+  const [roundWinners, setRoundWinners] = useState({});
   const [pairs, setPairs] = useState([]);
+  const [currentRoundScores, setCurrentRoundScores] = useState({});
 
   const tournamentRef = useRef(null);
   const game = useRef(null);
@@ -35,14 +44,46 @@ const SwissController = (props) => {
     e.preventDefault();
 
     let playerName = players.current.value;
+    let roundArray = [];
+
     if(playerInfo[playerName] === undefined) {
+      for(let i = 1; i <= Number(gameDetails.rounds); i++) {
+        roundArray.push(false)
+      }
+      setRoundWinners({...roundWinners, [playerName]: roundArray})
       setPlayerInfo({...playerInfo, [playerName]: 0})
     }
     players.current.value = '';
   }
 
+
   const handlePairings = (e) => {
     e.preventDefault();
+
+    let roundArray = [];
+    let newRoundWinners = {};
+    for(let i = 1; i <= Number(gameDetails.rounds); i++) {
+      if(gameDetails.currentRound === i) {
+        roundArray.push(true)
+      } else {
+        roundArray.push(false)
+      }
+    }
+
+    for(let i = 0; i < pairs.length; i++) {
+      let firstPlayer = pairs[i][0];
+      let secondPlayer = pairs[i][1];
+      let winner =
+        currentRoundScores[firstPlayer] > currentRoundScores[secondPlayer]
+        ? firstPlayer
+        : secondPlayer;
+      console.log(`Round ${gameDetails.currentRound} winner is ${winner} with ${currentRoundScores[winner]} points`)
+
+      newRoundWinners[winner] = roundArray;
+    }
+    setRoundWinners({...roundWinners, ...newRoundWinners});
+
+    // set new pairs
     let newPairs = [];
     const sorted =
       Object.entries(playerInfo)
@@ -57,13 +98,17 @@ const SwissController = (props) => {
     }
   }
 
+  // called every time score is added to player block
   const handleScoreUpdate = (e, player) => {
     e.preventDefault();
+    // set cumulative score
     let currentScore = parseInt(listRefs.get(player).current.value);
     if(currentScore) {
       let newScore = playerInfo[player] + currentScore;
       setPlayerInfo({...playerInfo, [player]: newScore});
     }
+    // set current round scores
+    setCurrentRoundScores({...currentRoundScores, [player]: currentScore})
     listRefs.get(player).current.value = '';
   }
 
@@ -119,7 +164,6 @@ const SwissController = (props) => {
                     ? <h2>It's a tie!</h2>
                     : ''
                 }
-
               </Container>
             </div>
           : <div>
@@ -163,11 +207,41 @@ const SwissController = (props) => {
                     key={index}
                     className="swiss-player"
                   >
+                  <Grid item sm={3}>
                     <p>{player} - {playerInfo[player]}</p>
+                  </Grid>
+                  <Grid item sm={5}>
+                    <TableContainer>
+                      <Table aria-label="simple table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Rounds</TableCell>
+                          { roundWinners[player].map((round, index) => {
+                            return <TableCell align="right">{index + 1}</TableCell>
+                          })}
+                        </TableRow>
+                      </TableHead>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell component="th" scope="row">
+                              Wins/Losses
+                            </TableCell>
+                            { roundWinners[player].map((round, index) => {
+                              return round === true
+                                ? <TableCell align="right">O</TableCell>
+                                : <TableCell align="right">X</TableCell>
+                            })}
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </Grid>
+                  <Grid item sm={4} align="right">
                     <form onSubmit={(e) => handleScoreUpdate(e, player)}>
                       <TextField label="add to score" variant="outlined" size="small" inputRef={listRefs.get(player)} />
                       <Button variant="contained" type="submit">add to score</Button>
                     </form>
+                  </Grid>
                   </Grid>
                 )
               })}
