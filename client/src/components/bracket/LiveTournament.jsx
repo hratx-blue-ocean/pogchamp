8b798951a25ad345cb4c67a08f43383d2b44545c
Emmanuel Garcia <earturoga@gmail.com>
2020-11-04 14:57:24 -0700

    some small changes

--- client/src/components/bracket/LiveTournament.jsx
+++ client/src/components/bracket/LiveTournament.jsx
@@ -4,11 +4,12 @@ function LiveTournament({players}) {
   return (
     <React.Fragment>
       <h3>Tournament View</h3>
+      {/* Money prize here */}
       <h1>Live Bracket Tournament</h1>
-      {players.length === 0 && players.length === 1 &&  
+      {players.length === 0 || players.length === 1 ?  
         <div style={{height: 550}}>
-        <h4>New tournament will display here</h4>
-      </div>}
+        <h4 style={{color: "grey"}}>Will show tournament here</h4>
+      </div> : null}
     </React.Fragment>
   );
 }
