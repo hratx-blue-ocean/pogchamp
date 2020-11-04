81e984903212deefcb8a8943421c922e106df3aa
Emily White <13emwhite13@gmail.com>
2020-11-04 16:11:24 -0600

    added basic winner placeholders

--- client/src/components/bracket/LiveTournament.jsx
+++ client/src/components/bracket/LiveTournament.jsx
@@ -4,9 +4,19 @@ function LiveTournament({players}) {
   return (
     <React.Fragment>
       <h3>Tournament View</h3>
+
       {/* Money prize here */}
+      {players.length === 1 ?
+      '' :
+        <div>
+          <p>First Place:</p>
+          <p>Second Place:</p>
+          <p>Third Place:</p>
+        </div>
+      }
+
       <h1>Live Bracket Tournament</h1>
-      {players.length === 0 || players.length === 1 ?  
+      {players.length === 0 || players.length === 1 ?
         <div style={{height: 550}}>
         <h4 style={{color: "grey"}}>Will show tournament here</h4>
       </div> : null}
