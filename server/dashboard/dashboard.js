7e7bb509377558e1478cc16ef8825c90c096d508
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-11-08 18:31:04 -0600

    Comment out dashboard.js

--- server/dashboard/dashboard.js
+++ server/dashboard/dashboard.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
-const { getUserData } = require('../../db/index.js');
-//need to deliver the information to the front end!
+// const express = require('express');
+// const router = express.Router();
+// const axios = require('axios');
+// const { getUserData } = require('../../db/index.js');
+// //need to deliver the information to the front end!
 
-router.get('/player', (req, res)=> {
-  console.log('Something coming in to organizer');
-  getUserData("thirdplayer", (err, result)=> {
-    if (err) {
-      console.log("error:", err);
-    } else {
-      console.log("answer:", result);
-      res.send(result)
-    }
-  })
-  // res.send({"userdata": "My name is mario"});
-})
-
-
+// router.get('/player', (req, res)=> {
+//   console.log('Something coming in to organizer');
+//   getUserData("thirdplayer", (err, result)=> {
+//     if (err) {
+//       console.log("error:", err);
+//     } else {
+//       console.log("answer:", result);
+//       res.send(result)
+//     }
+//   })
+//   // res.send({"userdata": "My name is mario"});
+// })
 
 
 
-module.exports = router;
+
+
+// module.exports = router;
